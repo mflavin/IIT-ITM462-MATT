0ff6570d5d1f5290ffd5e5a9705de7ac0c8a007e
mflavin <mflavin@hawk.iit.edu>
2017-09-16 02:15:00 -0500

    Stores all possible ranks into a var. Maps the JSON file in a var. Trying to set up check to see what matches.

--- Assignments/Week4/app.js
+++ Assignments/Week4/app.js
@@ -1,19 +1,21 @@
-$.getJSON("hand.json", function(card) {
+$.getJSON("hand.json", function(cards) {
   var $PossibleRanks = ["ace", "two", "three", "four", "five", "six", 
                         "seven", "eight", "nine", "ten", "jack", "queen", "king"];
-  var $ranks;
-  var $match = false;
   
-  $ranks = hand.map(function (cardRank) {
-    return card.rank;
+  var $inHand = cards.map(function (hand) {
+    return hand.rank;
   });
   
-  $ranks.forEach(function ($ranks) {
-    if(containsNTimes(PossibleRanks, ranks, 2)) {
-      $match = true;
+  $PossibleRanks.forEach(function(rank) {
+    var i = $inHand.length - 1;  
+    for(i; i >= 0; i--) {
+      var p = $PossibleRanks.length - 1;
+      for(p; p >= 0; p--) {
+        if($inHand[i] === $PossibleRanks[p]) {
+          console.log($inHand[i] + " " + $PossibleRanks[p]);
+          console.log("MATCH!");
+        }
+      }
     }
-    
-    console.log("A pair has been found!");
-  };
-  
+  });
 });
