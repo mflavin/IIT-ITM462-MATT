78d29ab258dd61486f06c798b3603b829a05171d
mflavin <mflavin@hawk.iit.edu>
2017-10-11 01:25:38 -0500

    Forgot to make smaller commits, but this makes two new hands. Makes two get request for the original hand. Makes a post request to add a new hand to local storage. Makes a put request to change that newly make hand in local storage.

--- Assignments/Week7/index.js
+++ Assignments/Week7/index.js
@@ -1,23 +1,52 @@
 var express = require('express');
 var app = express();
+var fs = require("fs"); 
 
 var port = 5000;
-var id = 1;
-var test = [
-{"suit" : "spades", "rank" : "3"},
-{"suit" : "hearts", "rank" : "ace"},
-{"suit" : "diamonds", "rank" : "10"},
-{"suit" : "clubs", "rank" : "7"}
-]
-var fs = require("fs");
+var newhand = {"id" : 17 , "cards" : [
+  {"suit" : "hearts", "rank" : "2"},
+  {"suit" : "clubs", "rank" : "7"},
+  {"suit" : "diamonds", "rank" : "3"},
+  {"suit" : "diamonds", "rank" : "8"},
+  {"suit" : "hearts", "rank" : "q"}
+]}
+
+var updatehand = {"id" : 38, "cards" : [
+  {"suit" : "clubs", "rank" : "9"},
+  {"suit" : "diamonds", "rank" : "8"},
+  {"suit" : "spades", "rank" : "6"},
+  {"suit" : "hearts", "rank" : "j"},
+  {"suit" : "clubs", "rank" : "7"}
+]}
+
+var hand = JSON.parse(fs.readFileSync('morehand.JSON'));
  
-app.get('/', function(request, response) {
-  response.end("id: " + id);
+app.get('/hands/' + hand.id, function(request, response) {
+  if(hand !== ' ') {
+    response.status(200).end(JSON.stringify({id: hand.id, cards: hand.cards}, null, ' '));
+  }
+  else {
+    response.status(404).end("Hand Not Found");
+  } 
 });
 
-app.get('/hands/cards', function(request, response) {
-  response.status(200);
-  response.end("id: " + id + "\n\n" + "cards: " + fs.readFileSync('moretest.JSON'));
+app.get('/hands/' + hand.id + '/cards', function(request, response) {
+  if(hand !== ' ') {
+    response.status(200).end(JSON.stringify({cards: hand.cards}, null, ' '));
+  }
+  else {
+    response.status(404).end("Hand Not Found");
+  }
+});
+
+app.post('/hands', function(request, response) {
+  fs.writeFileSync('./newhand.json', JSON.stringify(newhand));
+  response.status(200).end(JSON.stringify({id: 12}, null, '\t'));
+});
+
+app.put('/hands/' + hand.id, function(request, response) {
+  fs.writeFileSync('./newhand.json', JSON.stringify(updatehand));
+  response.status(204).end();
 });
 
 app.listen(port, function() {
